dd172352501934b17fee39ca0e7fa2e522e54ce8
drusso97 <devonrusso21@gmail.com>
2023-12-30 13:19:43 -0500

    commit1

--- main.py
+++ main.py
@@ -9,10 +9,13 @@
 
 import csv
 
+# Initialize empty list to store package objects
+packages = []
+
 
 # Creating package class
 class Package:
-    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, special_notes):
+    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, special_notes=""):
         self.package_id = package_id
         self.address = address
         self.city = city
@@ -21,3 +24,27 @@ class Package:
         self.deadline = deadline
         self.weight = weight
         self.special_notes = special_notes
+
+
+# Define function to parse csv file and create package objects
+def create_package_objects():
+    with open("files/WGUPS_package_file.csv", "r") as package_file:
+        reader_variable = csv.reader(package_file, delimiter=",")
+        for row in reader_variable:
+            package_id = row[0]
+            address = row[1]
+            city = row[2]
+            state = row[3]
+            zip_code = row[4]
+            deadline = row[5]
+            weight = row[6]
+            special_notes = row[7]
+            package = Package(package_id, address, city, state, zip_code, deadline, weight, special_notes)
+            packages.append(package)
+
+
+create_package_objects()
+
+for package in packages:
+    print(package.package_id, package.address, package.city, package.zip_code, package.deadline, package.weight,
+          package.special_notes)
